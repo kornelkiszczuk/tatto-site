7e12e923b228589ecb0d30bc89a20cda2644c4c5
Sziru <korneloo123@gmail.com>
2023-02-07 21:23:59 +0100

    initial commit, working on navbar

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+  <Nav />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+const navigation = [
+    {
+        id: 1,
+        path: '/kursy',
+        title: 'kursy'
+    },
+    {
+        id: 2,
+        path: '/akademia',
+        title: 'o akademii'
+    },
+    {
+        id: 3,
+        path: '/społeczność',
+        title: 'społeczość'
+    },
+    {
+        id: 4,
+        path: '/galeria',
+        title: 'galeria'
+    },
+    {
+        id: 5,
+        path: '/blog',
+        title: 'blog'
+    },
+    {
+        id: 6,
+        path: '/kontakt',
+        title: 'kontakt'
+    }
+]
+
+</script>
+
+
+<template>
+    <header>
+        <nav class="navbar">
+            <div class="navbar__logo">
+                <a href="#"><img src="../assets/logo.svg" alt="logo"></a>
+                <div class="navbar__navigation-container">
+                    <div class="navbar__navigation">
+                        <a v-for="link in navigation" :key="link.id" :href="link.path">{{ link.title }}</a>
+                    </div>
+                    <div class="navbar__contact">
+                        <p>KONTAKT.INQ@GMAIL.COM</p>
+                        <p>+48 601 150 899</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    .navbar {
+
+
+        // &__logo {
+        //     // mix-blend-mode: difference;
+        // }
+    }
+}
+</style>
